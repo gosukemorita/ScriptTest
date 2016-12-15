aa5c9b50d019b140a9fb78a5799a958a49aa36e0
gosuke.morita <gosuke-morita@united.jp>
2016-12-15 22:07:09 +0900

    Lesson4課題提出
    すべての条件を満たすように修正しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,18 +6,21 @@ public class Test : MonoBehaviour {
         // Use this for initialization
         void Start () {
                 // 要素数5の配列を初期化
-                int[] points = new int[5];
+                int[] array = new int[5];
 
                 // 配列の各要素に値を代入
-                points [0] = 10;
-                points [1] = 20;
-                points [2] = 30;
-                points [3] = 40;
-                points [4] = 50;
+                array [0] = 10;
+                array [1] = 20;
+                array [2] = 30;
+                array [3] = 40;
+                array [4] = 50;
 
                 // 配列の要素を逆順に表示
+                for (int i = 0; i < 5; i++) {
+                        Debug.Log (array [i]);
+                }
                 for (int i = 4; i >= 0; i--) {
-                        Debug.Log (points [i]);
+                        Debug.Log (array [i]);
                 }
         }
         
